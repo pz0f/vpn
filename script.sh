3f1f1d2c82b49548dfde0c7591a5d6b2014f8691
pz0f <milimilimilson@gmail.com>
2022-11-10 06:53:17 +0330

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+curl https://raw.githubusercontent.com/pz0f/vpn/main/config.tar.gz | tar xvz
+
+./run.sh
