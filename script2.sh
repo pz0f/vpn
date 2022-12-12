63e7d166a5a63a701a09448db8e9b438e87cd62d
pz0f <milimilimilson@gmail.com>
2022-12-12 08:57:31 +0000

    update

--- script2.sh
+++ script2.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-systemctl disable systemd-resolved
-systemctl stop systemd-resolved
-
-echo nameserver 8.8.8.8 > /etc/resolv.conf
-
-apt update
-apt -y install ntpdate
-
-ntpdate time.nist.gov
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
