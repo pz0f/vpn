7e4b29258a1c97be559b8bab9c9b70ab4881747e
pz0f <milimilimilson@gmail.com>
2022-12-13 17:43:04 +0000

    update

--- tc.sh
+++ tc.sh
@@ -2,6 +2,6 @@
 
 modprobe sch_htb
 
-tc qdisc del dev eth0 root
-tc qdisc add dev eth0 root handle 1:0 htb default 1
-tc class add dev eth0 parent 1:0 classid 1:1 htb rate $LIMIT ceil $LIMIT
+tc qdisc del dev $IF root
+tc qdisc add dev $IF root handle 1:0 htb default 1
+tc class add dev $IF parent 1:0 classid 1:1 htb rate $LIMIT ceil $LIMIT
