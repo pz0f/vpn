77006d3336cd214849f0b44a966062ebc151b501
pz0f <milimilimilson@gmail.com>
2022-12-14 14:06:18 +0000

    Update net_up.sh

--- net_up.sh
+++ net_up.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 IF=eth0
-LIMIT=500kbps
+LIMIT=1mbps
 
 rm /etc/resolv.conf
 echo nameserver 8.8.8.8 > /etc/resolv.conf
 
 modprobe sch_htb
+modprobe sch_sfq
 
-tc qdisc del dev $IF root
-tc qdisc add dev $IF root handle 1:0 htb default 1
-tc class add dev $IF parent 1:0 classid 1:1 htb rate $LIMIT ceil $LIMIT
+tc qdisc delete dev $IF root
+
+tc qdisc add dev $IF root handle 1: htb default 12
+tc class add dev $IF classid 1:1 htb rate $LIMIT ceil $LIMIT
+tc filter add dev $IF parent 1: protocol ip handle 6 fw flowid 1:1
+
+tc qdisc add dev $IF parent 1:1 handle 101: sfq perturb 20 flows 3000 limit 3000 depth 3000
+tc filter add dev $IF parent 101: protocol ip handle 10 flow hash keys dst divisor 1024
+
+iptables -t mangle -A PREROUTING -j MARK --set-mark 6
