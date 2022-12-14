35c963c67676d3a00872a5c84261e9a10a808d32
pz0f <milimilimilson@gmail.com>
2022-12-14 16:36:19 +0000

    Update tc.sh

--- tc.sh
+++ tc.sh
@@ -12,4 +12,5 @@ tc filter add dev $IF parent 1: protocol ip handle 6 fw flowid 1:1
 tc qdisc add dev $IF parent 1:1 handle 101: sfq perturb 20 flows 3000 limit 3000 depth 3000
 tc filter add dev $IF parent 101: protocol ip handle 10 flow hash keys dst divisor 1024
 
+iptables -t mangle -F
 iptables -t mangle -A PREROUTING -j MARK --set-mark 6
