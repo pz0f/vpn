9b924fba511ce0be2f2c14439087f8939142eb4b
pz0f <milimilimilson@gmail.com>
2022-12-14 14:04:13 +0000

    Update tc.sh

--- tc.sh
+++ tc.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 modprobe sch_htb
+modprobe sch_sfq
 
-tc qdisc del dev $IF root
-tc qdisc add dev $IF root handle 1:0 htb default 1
-tc class add dev $IF parent 1:0 classid 1:1 htb rate $LIMIT ceil $LIMIT
+tc qdisc delete dev $IF root
+
+tc qdisc add dev $IF root handle 1: htb default 12
+tc class add dev $IF classid 1:1 htb rate $LIMIT ceil $LIMIT
+tc filter add dev $IF parent 1: protocol ip handle 6 fw flowid 1:1
+
+tc qdisc add dev $IF parent 1:1 handle 101: sfq perturb 20 flows 3000 limit 3000 depth 3000
+tc filter add dev $IF parent 101: protocol ip handle 10 flow hash keys dst divisor 1024
+
+iptables -t mangle -A PREROUTING -j MARK --set-mark 6
