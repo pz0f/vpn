d0cf8fcb687e4ba0236bbd1328a389b24bad92b6
pz0f <milimilimilson@gmail.com>
2022-12-16 20:12:44 +0000

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -12,11 +12,11 @@ apt -y install ntpdate
 ntpdate time.nist.gov
 
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sh get-docker.sh
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 curl https://raw.githubusercontent.com/pz0f/vpn/main/config2.tar | tar xv
 
