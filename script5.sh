fa07179c3dfc4750da9d20399cc323a1bd6bd1c0
pz0f <milimilimilson@gmail.com>
2023-01-03 16:27:29 +0000

    Create script5.sh

---
+++ script5.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+systemctl disable systemd-resolved
+systemctl stop systemd-resolved
+
+rm /etc/resolv.conf
+echo nameserver 8.8.8.8 > /etc/resolv.conf
+
+apt update
+apt -y install ntpdate
+
+ntpdate time.nist.gov
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+curl -L "https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+curl https://raw.githubusercontent.com/pz0f/vpn/main/backup.tar.gz | tar xvz
+
+./run.sh
+
+curl -s -o /etc/network/if-up.d/net_up.sh https://raw.githubusercontent.com/pz0f/vpn/main/net_up.sh
+chmod +x /etc/network/if-up.d/net_up.sh
+/etc/network/if-up.d/net_up.sh
